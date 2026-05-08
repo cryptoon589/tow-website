1e1713d71c00682251d9be01eda5b85e57815f2c
cryptoon <crypt589n@gmail.com>
2026-05-08 17:30:17 -0600

    fix new nft folder

--- config/nfts.ts
+++ config/nfts.ts
@@ -9,37 +9,37 @@ export const nfts: NFTItem[] = [
   {
     id: "1",
     name: "TOW #1",
-    image: "public/assets/nft/1.png",
+    image: "public/assets/nfts/1.png",
     url: "https://xrp.cafe/nft/YOUR_ID",
   },
   {
     id: "2",
     name: "TOW #2",
-    image: "public/assets/nft/2.png",
+    image: "public/assets/nfts/2.png",
     url: "https://xrp.cafe/nft/YOUR_ID",
   },
   {
     id: "3",
     name: "TOW #3",
-    image: "public/assets/nft/3.png",
+    image: "public/assets/nfts/3.png",
     url: "https://xrp.cafe/nft/YOUR_ID",
   },
   {
     id: "4",
     name: "TOW #4",
-    image: "public/assets/nft/4.png",
+    image: "public/assets/nfts/4.png",
     url: "https://xrp.cafe/nft/YOUR_ID",
   },
   {
     id: "5",
     name: "TOW #5",
-    image: "public/assets/nft/5.png",
+    image: "public/assets/nfts/5.png",
     url: "https://xrp.cafe/nft/YOUR_ID",
   },
   {
     id: "6",
     name: "TOW #6",
-    image: "public/assets/nft/6.png",
+    image: "public/assets/nfts/6.png",
     url: "https://xrp.cafe/nft/YOUR_ID",
   },
 ];
