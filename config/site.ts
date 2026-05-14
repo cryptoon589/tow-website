85261f5bd5cfd371d6dd60354b9738eb5dcae449
cryptoon <crypt589n@gmail.com>
2026-05-14 07:58:18 -0600

    fix start game link

--- config/site.ts
+++ config/site.ts
@@ -21,7 +21,7 @@ export const siteConfig = {
 
 export const navItems = [
   { label: "Home", href: "/" },
-  { label: "Play Game", href: "/play" },
+  { label: "Play Game", href: "/play/start" },
   { label: "Make Memes", href: "/meme-generator" },
   { label: "Everyone's Tired", href: "/raid-board" },
   { label: "NFTs", href: "/nft-collection" },
