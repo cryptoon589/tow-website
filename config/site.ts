edb79a0158d49639342d1e7947ea65422448b295
cryptoon589 <crypt589n@gmail.com>
2026-05-26 12:15:33 -0600

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -21,8 +21,8 @@ export const siteConfig = {
 
 export const navItems = [
   { label: "Home", href: "/" },
-  { label: "Play Game", href: "/play/start" },
   { label: "Too Tired To Quit", href: "/too-tired-to-quit" },
+  { label: "Play Game", href: "/play/start" },
   { label: "Make Memes", href: "/meme-generator" },
   { label: "Everyone's Tired", href: "/raid-board" },
   { label: "NFTs", href: "/nft-collection" },
