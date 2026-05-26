98bbf86df940ffc1a91cf6df6dddd617809b1bc5
cryptoon589 <crypt589n@gmail.com>
2026-05-26 12:09:16 -0600

    Update page.tsx

--- app/play/start/page.tsx
+++ app/play/start/page.tsx
@@ -80,8 +80,10 @@ export default function PlayStartPage() {
             TOW
           </Link>
 
-          <p className="px-3 py-1 text-xs font-black">Too Tired To Quit</p>
-        </header>
+          <Link
+            href="/" 
+            className="px-3 py-1 text-xs font-black">Too Tired To Quit</p>
+        </Link>
 
         <section className="grid min-h-[calc(100vh-72px)] gap-4 lg:grid-cols-[0.92fr_1.08fr] lg:items-start">
           <div className="flex flex-col gap-4">
